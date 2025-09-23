90f553afed2b1a9d953a5a5889ed8378e739ce91
MyPartsRunner <mypartsrunner@example.com>
2025-09-23 03:45:06 -0400

    Fix JSX syntax error in AuthModal component

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -197,6 +197,7 @@ const AuthModal: React.FC<AuthModalProps> = ({ isOpen, onClose, onSuccess }) => 
             </form>
           </TabsContent>
         </Tabs>
+        </div>
       </DialogContent>
     </Dialog>
   );
