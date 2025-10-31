6a911e4a4b8a1750112e5c3d63eeca927eb819ba
MyPartsRunner <mypartsrunner@example.com>
2025-10-31 17:56:23 -0400

    Adjust hero logo vertical position to better align with heading text

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -91,7 +91,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({ onRequestPickup, onBecomeDriv
 
           <div className="relative">
             {/* Hero Logo */}
-            <div className="relative w-full h-[30rem] flex items-start justify-center pt-8">
+            <div className="relative w-full h-[30rem] flex items-start justify-center pt-2">
               <img 
                 src="/hero-logo.png"
                 alt="MyPartsRunner Logo"
